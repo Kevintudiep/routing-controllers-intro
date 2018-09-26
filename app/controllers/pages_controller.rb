5ea71259f6398c8630bab3fc184d27b7f3c704f8
kevintudiep <kevintudiep@gmail.com>
2018-09-26 15:05:25 -0500

    Dynamic and redirecting routes added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,33 @@
 class PagesController < ApplicationController
+
+before_action :set_kitten_url, only: [:kitten, :kittens]
+
+  def welcome
+    @header = "This is the Welcome Page!"
+    render :about
+  end
+
+  def about
+    @header = "i love dogs!"
+  end
+
+  def contest
+    flash[:notice] = "Sorry, the contest has ended"
+    redirect_to '/welcome'
+  end
+
+
+  def kitten
+  end
+
+  def kittens
+  end
+
+
+
+  def set_kitten_url
+   requested_size = params[:size]
+   @kitten_url = "http://placekitten.com/#{requested_size}/#{requested_size}"
+  end
+
 end
